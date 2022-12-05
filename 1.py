d25dbe20e9ad04be94591590ee6c89eac010483d
Gabriel Melhor <melhorgabriel11@gmail.com>
2022-12-05 09:48:39 -0300

    Desafio finalizado

---
+++ 1.py
@@ -0,0 +1,17 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
+number = input("Digite um numero: ")
+if number.isdigit():
+    numberInteger = int(number)
+    par_impar = numberInteger % 2 == 0
+    par_impar_texto = "impar" 
+
+    if par_impar:
+        par_impar_texto = "par"
+    print(f"O número {numberInteger} é {par_impar_texto}")
+else:
+    print("Você não digitou um número inteiro")
